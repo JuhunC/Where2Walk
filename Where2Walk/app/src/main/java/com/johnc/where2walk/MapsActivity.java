762845bc94496afefed164353b33218ff787f99c
JuHunChoi <a71790890@gmail.com>
2018-02-02 23:36:07 +0900

    맵 화면(자동 위치 찾아서 표시)

--- Where2Walk/app/src/main/java/com/johnc/where2walk/MapsActivity.java
+++ Where2Walk/app/src/main/java/com/johnc/where2walk/MapsActivity.java
@@ -1,19 +1,44 @@
 package com.johnc.where2walk;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+import java.util.Locale;
 
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    final int REQUEST_CODE_LOCATION = 1;
     private GoogleMap mMap;
+    private Location mLoc;
+    LocationManager mlocationManager;
+    LocationListener mlocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        settingGPS();
+        mLoc = getMyLocation();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
@@ -25,10 +50,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Seoul
+        LatLng Seoul = new LatLng(mLoc.getLatitude(),mLoc.getLongitude());
+        //LatLng Seoul = new LatLng(37.54, 126.99);
+        mMap.addMarker(new MarkerOptions().position(Seoul).title("Marker in Seoul"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Seoul,18));
     }
+    private void settingGPS(){
+        mlocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mlocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                // TODO 위도, 경도로 하고 싶은 것
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+    }
+    private Location getMyLocation() {
+        Location currentLocation = null;
+        // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 사용자 권한 요청
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, this.REQUEST_CODE_LOCATION);
+        }
+        else {
+            mlocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mlocationListener);
+            mlocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocationListener);
+
+            // 수동으로 위치 구하기
+            String locationProvider = LocationManager.GPS_PROVIDER;
+            currentLocation = mlocationManager.getLastKnownLocation(locationProvider);
+            if (currentLocation != null) {
+                double lng = currentLocation.getLongitude();
+                double lat = currentLocation.getLatitude();
+                Log.d("Main", "longtitude=" + lng + ", latitude=" + lat);
+            }
+        }
+        return currentLocation;
+    }
+
 }
